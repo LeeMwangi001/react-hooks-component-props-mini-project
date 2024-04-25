40897f6855dab6302c152cc6e6443b5350c0b73d
Lee <lee.mugweru@gmail.com>
2024-04-26 00:47:16 +0300

    Completes lab

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,10 +1,22 @@
 import React from "react";
 
-function Article({ title, date = "January 1, 1970", preview }) {
+function Article({ title, date = "January 1, 1970", preview, minutes }) {
+    const renderEmojis = () => {
+        const emojis = [];
+        const numEmojis = minutes < 30 ? Math.ceil(minutes / 5) : Math.ceil(minutes / 10);
+        const emoji = minutes < 30 ? "☕️" : "🍱";
+
+        for (let i = 0; i < numEmojis; i++) {
+            emojis.push(emoji);
+        }
+
+        return emojis.join("");
+    };
+
     return (
         <article>
             <h3>{title}</h3>
-            <small>{date}</small>
+            <small>{renderEmojis()} {minutes} min read</small>
             <p>{preview}</p>
         </article>
     );
